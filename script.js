3844d51b8cb8226e5036044170c2afa4dfecd76b
Amar Ganesh Pandian <109662654+amardotjs@users.noreply.github.com>
2024-09-02 11:28:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function yes(){
+    window.open("https://c.tenor.com/pPnM7wANoVoAAAAC/tenor.gif")
+}
+function no(){
+    window.alert("Ok it is fine :) lets be friends adhuku nu don't brozone me lol")
+}
